cc9678221f03de6c814456a07f60c67f6c11e076
brightwang <brightwang1984@gmail.com>
2014-05-30 17:08:07 +0800

    创建查询Query类

---
+++ Gram/Db/Query.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * User: brightwang
+ * Date: 14-5-30
+ * Time: 下午4:56
+ */
+
+class Query
+{
+    public function where()
+    {
+        return $this;
+    }
+
+    public function first()
+    {
+        return $this;
+    }
+
+    public function limit()
+    {
+
+    }
+}
