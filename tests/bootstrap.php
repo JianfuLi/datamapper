c826c32644ce7512fcb309184720b7c510eb8cd4
lijf <me@ljf.me>
2014-09-24 16:22:27 +0800

    增加单元测试引导文件

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,11 @@
 <?php
- /**
+/**
  *
  *
  * @version   1.0
  * @author    jianphu (me@ljf.me)
  * @create    2014/9/24 16:19
  * @copyright Copyright (c) 2014 (http://ljf.me)
- */ 
+ */
+$loader = require '../vendor/autoload.php';
+$loader->add('Gram\DataMapper', '../src');
